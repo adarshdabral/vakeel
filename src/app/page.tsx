25b0931d3062b3ee7630d15dc883649e4a6160f0
adarshdabral <106724982+adarshdabral@users.noreply.github.com>
2024-06-30 20:28:30 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,3 +8,20 @@ export default function Home() {
    </main>
   );
 }
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply the CSP to all routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'",
+          },
+        ],
+      },
+    ];
+  },
+};
